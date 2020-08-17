ff07ad86837167ff3330764e1d8e69a6cb7634bf
Henrique T. Kato <henrikato@MacBook-Pro-do-Kato.local>
2020-08-17 17:18:55 -0300

    tira validacao por hora para testes

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -2,15 +2,14 @@ import React, { useRef, useState } from 'react';
 import { View, Alert, TouchableOpacity, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
-import * as yup from 'yup';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
-import Container from '@components/Container';
-import FormInput from '@components/FormInput';
-import FlexImage from '@components/FlexImage';
-import Button from '@components/Button';
+import Container from 'components/Container';
+import FormInput from 'components/FormInput';
+import FlexImage from 'components/FlexImage';
+import Button from 'components/Button';
 
-import logo from '@assets/logo.png'
+import logo from 'assets/logo.png'
 
 export default () => {
   const navigation = useNavigation();
@@ -22,22 +21,22 @@ export default () => {
   const [hidePassword, setHidePassword] = useState(true);
 
   function _handleSubmit(data) {
-    const schema = yup.object().shape({
-      login: yup.string().email("Informe um e-mail válido").required("Informe o seu login"),
-      senha: yup.string().min(6, "A senha possui no mínimo 6 caracteres.").required("Informe a sua senha")
-    });
+    // const schema = yup.object().shape({
+    //   login: yup.string().email("Informe um e-mail válido").required("Informe o seu login"),
+    //   senha: yup.string().min(6, "A senha possui no mínimo 6 caracteres.").required("Informe a sua senha")
+    // });
 
-    schema.validate(data, { abortEarly: false })
-      .then(({login, senha}) => {
-        Alert.alert("Login", `Login: ${login}\nSenha: ${senha}`);
-      })
-      .catch(err => {
-        const validationErrors = {};
-        if(err instanceof yup.ValidationError) {
-          err.inner.forEach(erro => { validationErrors[erro.path] = erro.message });
-          _form.current.setErrors(validationErrors);
-        }
-      });
+    // schema.validate(data, { abortEarly: false })
+    //   .then(data => {
+        navigation.reset({ index: 0, routes: [{name: "main"}] })
+      // })
+      // .catch(err => {
+      //   const validationErrors = {};
+      //   if(err instanceof yup.ValidationError) {
+      //     err.inner.forEach(erro => { validationErrors[erro.path] = erro.message });
+      //     _form.current.setErrors(validationErrors);
+      //   }
+      // });
   }
 
   return (
@@ -47,8 +46,8 @@ export default () => {
           <FlexImage source={logo} style={{flexGrow: .5, width: 250, alignSelf: "center"}} />
 
           <Form ref={_form} onSubmit={_handleSubmit}>
-            <FormInput label="Login" name="login" keyboardType="email-address" helper="Informe o seu e-mail de login" />
-            <FormInput label="Senha" name="senha" secureTextEntry={hidePassword} helper="Informe a sua senha">
+            <FormInput label="Login" name="login" keyboardType="email-address" autoCompleteType="email" helper="Informe o seu e-mail de login" />
+            <FormInput label="Senha" name="senha" secureTextEntry={hidePassword} autoCompleteType="password" helper="Informe a sua senha">
               <TouchableOpacity onPress={() => setHidePassword(!hidePassword)} hitSlop={{top: 10, right: 10, bottom: 10, left: 10}}>
                 <FontAwesomeIcon icon={hidePassword ? "eye" : "eye-slash"} />
               </TouchableOpacity>
@@ -63,7 +62,11 @@ export default () => {
               LOGIN
             </Button>
 
-            <Button onPress={() => _form.current.submitForm()}>ESQUECI MINHA SENHA</Button>
+            <Button style={{backgroundColor: "#FAFAFA"}} 
+              labelStyle={{color: "#222"}}
+              onPress={() => _form.current.submitForm()}>
+                ESQUECI MINHA SENHA
+            </Button>
           </View>
         </Container>
       </ScrollView>
